413be8fe288b18bbfea0c56b08929117358ed7e3
seohyun-lee <applemint2003@gmail.com>
2024-05-23 05:48:10 +0900

    상품 주문하기 기능, 선택한 주문의 상세 정보 보기 기능

---
+++ JAVA_Files/order/Order.java
@@ -0,0 +1,36 @@
+package JAVA_Files.order;
+
+import JAVA_Files.MainPage;
+import JAVA_Files.util.DatabaseConnection;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.time.LocalDateTime;
+
+public class Order {
+
+    public static void createOrder(int shoesOptionId, String deliveryAddress, int orderPrice, String paymentType) {
+        try (Connection conn = DatabaseConnection.getConnection();
+             PreparedStatement pstmt = conn.prepareStatement("INSERT INTO Orders values(?, ?, ?, ?, ?, ?, ?)");
+        ) {
+            int userId = MainPage.loggedInUserId; //현재 로그인된 유저 아이디 가져오기
+            pstmt.setInt(1, userId);
+            pstmt.setInt(2, shoesOptionId);
+            pstmt.setTimestamp(3, java.sql.Timestamp.valueOf(LocalDateTime.now()));
+            pstmt.setString(4,deliveryAddress);
+            pstmt.setString(5, "Processing");
+            pstmt.setInt(6, orderPrice);
+            pstmt.setString(7, paymentType);
+            ResultSet rs = pstmt.executeQuery();
+            if (rs.next()) {
+
+            } else {
+
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+}
