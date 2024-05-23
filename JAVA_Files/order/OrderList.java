9c47f1f47aca097644a2f51208c986b415d2b95a
seohyun-lee <applemint2003@gmail.com>
2024-05-24 07:17:09 +0900

    feat: OrderList 기능 개발

--- JAVA_Files/order/OrderList.java
+++ JAVA_Files/order/OrderList.java
@@ -7,31 +7,48 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.time.LocalDateTime;
+import java.util.Scanner;
 
 public class OrderList {
     public static void orderList(int userId) { //현재 로그인된 유저 아이디를 받는다
+        Scanner scanner = new Scanner(System.in);
         ShopUI.printOrderListBanner();
 
-        String sql = ""; // View
+        // View를 사용한 쿼리
+        String sql = "SELECT order_id, shoes_name, size_number, ordered_at, delivery_status, order_price " +
+                "FROM OrderSummary WHERE user_id = ?";
         try (Connection conn = DatabaseConnection.getConnection();
              PreparedStatement pstmt = conn.prepareStatement(sql);
         ) {
             pstmt.setInt(1, userId);
 
             ResultSet rs = pstmt.executeQuery();
+            boolean isEmpty = true;
             while (rs.next()) {
-                String shoesName = rs.getString("name");
+                isEmpty = false;
+                int orderId = rs.getInt("order_id");
+                String shoesName = rs.getString("shoes_name");
                 int sizeNumber = rs.getInt("size_number");
-                int price = rs.getInt("price");
                 String orderedAt = rs.getTimestamp("ordered_at").toString();
-                String deliveryAddress = rs.getString("delivery_address");
-                String deliverStatus = rs.getString("delivery_status");
-                String paymentType = rs.getString("payment_type");
-                // Todo: 출력
+                String deliveryStatus = rs.getString("delivery_status");
+                int orderPrice = rs.getInt("order_price");
+                System.out.println(" 주문번호: " + orderId + " [주문일자: " + orderedAt + "]");
+                System.out.println("-------------------------------");
+                System.out.println(" 배송상태: " + deliveryStatus);
+                System.out.println("-------------------------------");
+                System.out.println(" 제품명: " + shoesName);
+                System.out.println(" 사이즈: " + sizeNumber + " | 1개");
+                System.out.println("-------------------------------");
+                System.out.println(" 결제금액: " + orderPrice + "원");
+                System.out.println("+—————————————————————————————+");
+            }
+            if (isEmpty) {
+                System.out.println("주문 내역이 없습니다.");
             }
         } catch (SQLException e) {
             e.printStackTrace();
         }
+        System.out.println("이전 페이지로 돌아가려면 아무 키나 누르세요.");
+        scanner.nextLine();
     }
 }
