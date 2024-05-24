dab9224d42fa1714d13e30e94e8467df7516b96b
Lee Hannah <mohan0226@naver.com>
2024-05-25 00:53:41 +0900

    DatabaseConnection {} 설정

--- JAVA_Files/util/DatabaseConnection.java
+++ JAVA_Files/util/DatabaseConnection.java
@@ -5,8 +5,8 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DatabaseConnection {
-    private static final String URL = "jdbc:mysql://localhost:3306/shoechema";
-    private static final String USER = "root";
+    private static final String URL = "jdbc:mysql://localhost:3306/{db명}";
+    private static final String USER = "{유저명}";
     private static final String PASSWORD = "{비밀번호}";
 
     public static Connection getConnection() throws SQLException {
