ddd4aa5d12dabb4a3e1a60f197088d87f409e715
seohyun-lee <applemint2003@gmail.com>
2024-05-25 21:35:52 +0900

    Hotfix: SearchProducts 사이즈 가짓수 출력하도록 변경, SQL문 변경

--- JAVA_Files/product/SearchProducts.java
+++ JAVA_Files/product/SearchProducts.java
@@ -12,12 +12,15 @@ import java.util.Scanner;
 // 상품 검색 페이지
 public class SearchProducts {
 
+    // 사용자가 신발 이름을 검색하면 해당되는 신발의 제품 넘버(shoes_id), 제품명, 가격, 사이즈 가짓수를 제시해주는 메서드.
     public static void searchProducts() {
         Scanner scanner = new Scanner(System.in);
         ShopUI.printSearchBanner(); // 검색 배너 출력
 
         //SQL문 작성
-        String sql = "SELECT * FROM Shoes WHERE shoes_name LIKE ?";
+        String sql = "SELECT Shoes.shoes_id, shoes_name, price, count(size_id) AS count FROM Shoes, ShoesOptions" +
+                " WHERE Shoes.shoes_id = ShoesOptions.shoes_id" +
+                " AND shoes_name LIKE ? GROUP BY Shoes.shoes_id, shoes_name, price";
 
         //PreparedStatement 지정
         try (Connection conn = DatabaseConnection.getConnection();
@@ -43,6 +46,7 @@ public class SearchProducts {
                     System.out.println(" [" + shoes.getShoesId() + "]");
                     System.out.println(" 제품명 : " + shoes.getShoesName());
                     System.out.println(" 가격 : " + shoes.getPrice() + "원");
+                    System.out.println(" (" + rs.getInt("count") + "가지 사이즈)");
                     System.out.println("--------------------------------");
                 }
             }
