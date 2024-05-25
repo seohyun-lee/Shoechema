69e17a5afb562cfa354287cddfa77b9bd79d6365
seohyun-lee <applemint2003@gmail.com>
2024-05-25 15:20:55 +0900

    commment: ShopUI 주석 달기 완료

--- JAVA_Files/UI/ShopUI.java
+++ JAVA_Files/UI/ShopUI.java
@@ -1,5 +1,6 @@
 package JAVA_Files.UI;
 
+// 문자열 출력을 통해 쇼핑몰 애플리케이션에서 페이지 및 기능들을 명확히 표시하고자 작성한 클래스
 public class ShopUI {
     // GuestPage, MainPage 배너
     public static void printShopBanner() { // 쇼핑몰 배너
@@ -25,7 +26,7 @@ public class ShopUI {
         System.out.println("|                     ~~~ 회원가입 ~~~                      |");
         System.out.println("+==========================================================+");
     }
-    public static void printUserProfileBanner() { // 유저 정보 배너
+    public static void printUserProfileBanner() { // 회원 정보 배너
         System.out.println("+==========================================================+");
         System.out.println("|                     ~~~ 회원 정보 ~~~                      |");
         System.out.println("+==========================================================+");
@@ -37,12 +38,12 @@ public class ShopUI {
     }
 
     // product 배너들
-    public static void printSearchBanner() { // 검색 배너
+    public static void printSearchBanner() { // 신발 검색 배너
         System.out.println("+==========================================================+");
         System.out.println("|                     ~~~ 신발 검색 ~~~                      |");
         System.out.println("+==========================================================+");
     }
-    public static void printSearchResultBanner() { // 검색 결과 배너
+    public static void printSearchResultBanner() { // 신발 검색 결과 배너
         System.out.println("+——————————————————————————————+");
         System.out.println("|         신발 검색 결과         |");
         System.out.println("+——————————————————————————————+");
@@ -63,7 +64,7 @@ public class ShopUI {
         System.out.println("+==========================================================+");
     }
 
-    // 주문 배너들
+    // order 배너들
     public static void printDoOrderBanner() { // 주문하기 배너
         System.out.println("+——————————————————————————————+");
         System.out.println("|            주문하기            |");
@@ -80,7 +81,7 @@ public class ShopUI {
         System.out.println("+——————————————————————————————+");
     }
     
-    // LoginPage의 프로모션 텍스트
+    // GuestPage의 프로모션 텍스트
     public static void printPromoText1() {
         System.out.println("      ~~~ Shoechema의 신발과 특별한 여정을 함께하세요 ~~~      ");
     }
